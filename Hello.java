7e180f803c1b3832d05d3f4a157bcb479a263439
Catherine Davis <34278437+Catherine-M-Davis@users.noreply.github.com>
2019-01-23 20:42:19 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -6,6 +6,6 @@ public class Hello {//open class
 	public static void main(Scanner keyboard){//opening main
 		System.out.println("Enter your name: ");
 		String name = keyboard.nextLine();
-		System.out.println("Name is: "+name);
+		System.out.println("You have entered your name as: "+name);
 	}//closing main
 }//close class
